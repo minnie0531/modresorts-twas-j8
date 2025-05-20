e5d64a6d269b901606cfccf8425a632011a11eb0
minhee-kim <minhee.kim@ibm.com>
2025-05-20 15:09:43 +0900

    add document for do get in logout

--- src/main/java/com/acme/modres/LogoutServlet.java
+++ src/main/java/com/acme/modres/LogoutServlet.java
@@ -13,6 +13,17 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Handles HTTP GET requests for the logout functionality.
+     *
+     * This method is called when a GET request is made to the logout endpoint.
+     * It revokes any existing Single Sign-On (SSO) cookies and then redirects
+     * the user to the login page.
+     *
+     * @param request the incoming HTTP request
+     * @param response the outgoing HTTP response
+     * @throws IOException if an I/O error occurs while sending the response
+     */
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws IOException {
